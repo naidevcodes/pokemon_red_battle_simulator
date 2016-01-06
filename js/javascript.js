68ad2b0b0a6ae008fc9a3ed12707f7d38c54f467
NathanAlexander <naidledoes@gmail.com>
2016-01-06 13:12:54 -0800

    Created both of the initial JS pokemon objects

---
+++ js/javascript.js
@@ -0,0 +1,63 @@
+var charmander {
+  name: "Billy",
+  health: 100,
+  lvl: 12,
+  effect: null,
+  moves: [{
+    name: "Ember",
+    type: "Attack",
+    power: 20,
+    accuracy: .80
+  }, 
+  {
+    name: "Scratch",
+    type: "Attack",
+    power: 10,
+    accuracy: .90
+  },
+  {
+    name: "Leer",
+    type: "Defense",
+    power: .20,
+    accuracy: 1.0
+  },
+  {
+    name: "Growl",
+    type: "Defense",
+    power: .65,
+    accuracy: .65
+  }]
+
+};
+
+var pikachu {
+  name: "Pikachu",
+  health: 100,
+  lvl: 11,
+  effect: null,
+  moves: [{
+    name: "Thunder Shock",
+    type: "Attack",
+    power: 10,
+    accuracy: .95
+  }, 
+  {
+    name: "Thunder Wave",
+    type: "Attack",
+    power: 25,
+    accuracy: .60
+  },
+  {
+    name: "Tail Whip",
+    type: "Defense",
+    power: .15,
+    accuracy: 1.0
+  },
+  {
+    name: "Growl",
+    type: "Defense",
+    power: .55,
+    accuracy: .65
+  }]
+
+};
